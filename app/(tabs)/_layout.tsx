6adc2cf59a2085cc5e61c749e9a7ea26c8f59b85
akdevv <akdev5520@gmail.com>
2025-09-08 01:17:12 +0530

    Update app configuration and layout structure; change user interface style to dark, enhance navigation with additional screens, and remove unused index file.

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,36 @@
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="stats"
+        options={{
+          title: "Stats",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+}
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View>
-      <Text className="text-2xl font-bold text-blue-500">Hello World</Text>
+      <Text>Home Page</Text>
     </View>
   );
 }
