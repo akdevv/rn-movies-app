8a1a8e010f1ed1b7c44eb1614cc8022704dba5bc
akdevv <akdev5520@gmail.com>
2025-09-08 17:10:48 +0530

    Add phosphor-react-native for tab icons; enhance TabsLayout with custom icons for Home, Find, Stats, and Profile screens.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,85 @@
 import { Tabs } from "expo-router";
+import {
+  CardsThree,
+  House,
+  MagnifyingGlass,
+  UserCircle,
+} from "phosphor-react-native";
+import { Text, View } from "react-native";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: React.ReactNode;
+  title: string;
+}) => {
+  return (
+    <View className="flex items-center justify-center gap-1 mt-4">
+      <View
+        className={`py-1 px-4 ${focused ? "bg-accent-primary rounded-full" : ""}`}
+      >
+        {icon}
+      </View>
+      <Text className="text-[10px] font-inter text-secondary-text">
+        {title}
+      </Text>
+    </View>
+  );
+};
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0D0D0F",
+          marginBottom: 0,
+          borderTopWidth: 0,
+          borderTopColor: "transparent",
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={<House size={20} color={focused ? "#0D0D0F" : "#A0A0A0"} />}
+              title="Home"
+            />
+          ),
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
-          title: "Search",
+          title: "Find",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <MagnifyingGlass
+                  size={20}
+                  color={focused ? "#0D0D0F" : "#A0A0A0"}
+                />
+              }
+              title="Find"
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -22,6 +87,15 @@ export default function TabsLayout() {
         options={{
           title: "Stats",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <CardsThree size={20} color={focused ? "#0D0D0F" : "#A0A0A0"} />
+              }
+              title="Stats"
+            />
+          ),
         }}
       />
       <Tabs.Screen
@@ -29,6 +103,15 @@ export default function TabsLayout() {
         options={{
           title: "Profile",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon
+              focused={focused}
+              icon={
+                <UserCircle size={20} color={focused ? "#0D0D0F" : "#A0A0A0"} />
+              }
+              title="Profile"
+            />
+          ),
         }}
       />
     </Tabs>
